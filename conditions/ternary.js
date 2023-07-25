88c9b262851d97a3e6fb5d51bd0be344a6135600
luca <lucagiustini92@gmail.com>
2023-07-25 06:34:10 -0700

    3nd html

---
+++ conditions/ternary.js
@@ -0,0 +1,10 @@
+const age = 15;
+const status = age >= 18 ? 'Adult' : 'Minor';
+
+console.log(status); // Output: 'Adult'
+
+const score = 75;
+const grade =
+  score >= 90 ? 'A' : score >= 80 ? 'B' : score >= 70 ? 'C' : 'F';
+
+console.log(grade); // Output: 'B'
