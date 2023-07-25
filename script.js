c65dcaab12ebab5ac7c835d6c771eb2dbdc659a2
luca <lucagiustini92@gmail.com>
2023-07-25 06:14:13 -0700

    2nd html

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript code to print "Hello, World!"
+document.addEventListener('DOMContentLoaded', function() {
+    var outputElement = document.getElementById('output');
+    outputElement.textContent = 'Hello, World!';
+  });
