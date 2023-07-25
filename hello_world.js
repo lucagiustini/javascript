d87c2c6431753e659f27fb5959b26c2214528852
luca <lucagiustini92@gmail.com>
2023-07-25 06:00:44 -0700

    1st js commit

---
+++ hello_world.js
@@ -0,0 +1,6 @@
+// create a script to print hello world
+
+console.log("Hello World");
+
+// to run it in browser, open the browser console and type: node hello_world.js
+// to run it in the terminal: node hello_world.js
